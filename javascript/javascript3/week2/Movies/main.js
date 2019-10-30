25d8ed7805226cddc8c16427b2af9e296942146a
mamathamereddy <mamathavangala1@gmail.com>
2019-10-30 14:08:49 +0100

    commit

--- javascript/javascript3/week2/Movies/main.js
+++ javascript/javascript3/week2/Movies/main.js
@@ -1,7 +1,7 @@
 
 //Movies exercise
   fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json')
-  
+
     .then(response => response.json())
     .then(movies => {
       const badMovies = movies.filter(movie => movie.rating < 4);
@@ -10,6 +10,6 @@
       const moviesafter2000 = badMovies.filter(badMovies => badMovies.year > 2000);
       console.log('Movies Science 2000 are:', moviesafter2000);
 
-      const titleBadMovies = badMovies.filter(badMovies => badMovies.title);
+      const titleBadMovies = badMovies.map(badMovies => badMovies.title);
       console.log('Bad Movies Titles are:', titleBadMovies);
     });
