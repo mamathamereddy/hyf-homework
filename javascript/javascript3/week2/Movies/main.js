b9091f5872a00b6102ef7109fc68dfe69c0e2f5a
mamathamereddy <mamathavangala1@gmail.com>
2019-10-30 14:06:22 +0100

    removed extra spaces

--- javascript/javascript3/week2/Movies/main.js
+++ javascript/javascript3/week2/Movies/main.js
@@ -1,6 +1,7 @@
 
 //Movies exercise
   fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json')
+  
     .then(response => response.json())
     .then(movies => {
       const badMovies = movies.filter(movie => movie.rating < 4);
@@ -12,17 +13,3 @@
       const titleBadMovies = badMovies.filter(badMovies => badMovies.title);
       console.log('Bad Movies Titles are:', titleBadMovies);
     });
-
-
-
-
-
-
-
-
-    
-
- 
-
-
-  
