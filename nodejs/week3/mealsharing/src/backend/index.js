1b15e6952b68d7b89ea43c87b6d1fa33dcdd3347
mamathamereddy <mamathavangala1@gmail.com>
2020-01-10 12:06:19 +0100

    nodejs week3 h/w

---
+++ nodejs/week3/mealsharing/src/backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+
+const mealsRouter = require("./api/meals");
+const reservationsRouter = require("./api/reservations");
+const reviewsRouter = require("./api/reviews");
+
+const port = process.env.PORT || 5000;
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+router.use("/meals", mealsRouter);
+router.use("/reservations",reservationsRouter);
+router.use("/reviews",reviewsRouter);
+
+
+app.use("/api", router);
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
