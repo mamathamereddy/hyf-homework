1cb53fae3cf9277e07d5afaf13d54dedb3574065
mamathamereddy <mamathavangala1@gmail.com>
2020-02-20 01:25:38 +0100

    midified searchuser component

--- react/week4/github-user-searcher/src/components/SearchUser.js
+++ react/week4/github-user-searcher/src/components/SearchUser.js
@@ -10,10 +10,7 @@ function SearchUser() {                //Parent
   const [error, setError] = useState("");
 
   useEffect(() => {
-    fetch(`https://api.github.com/search/users?q=${inputvalue}`,
-    {
-      headers: { Authorization: "Basic " + btoa("pugthenid:7eT28vVSfD") }
-    })
+    fetch(`https://api.github.com/search/users?q=${inputvalue}`)
       .then(res => res.json())
       .then(data => {
         setUsername(data.items);
@@ -36,98 +33,3 @@ function SearchUser() {                //Parent
 }
 
 export default SearchUser;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import React, { useState,useEffect } from 'react'
-
-function SearchUser() {
-
-  const [username, setUsername] = useState([])
-  const [inputvalue, setInputValue] = useState('')
-  const [loading, setLoading] = useState(true);
-  const[error,setError]=useState('')
-  
-  
-  /*const GetUserNames = (querie) => {
-    return (
-      fetch(`https://api.github.com/search/users?q=${querie}`)
-        .then(res => res.json())
-    )  
-  } */
- 
-  /*useEffect(() => {
-  /* GetUserNames(inputvalue)*/
-  /*fetch(`https://api.github.com/search/users?q=${inputvalue}`)
-  .then(res => res.json())
-          .then(data => {
-            setUsername(data.items);
-            setLoading(!loading);
-          })
-          .catch(err => {
-            setError("error") ;
-          });
-      },[inputvalue])
-
-  
-      
-  return (
-    <>
-      
-        <input
-          type="text"
-          value={inputvalue}
-        onChange={event => setInputValue(event.target.value)}
-        placeholder="Search for user"
-        required
-      />
-      <ul>
-        {loading ? "loading...." :   
-          username === undefined ? "No results" :
-            username.map(item => <li key={item.id}>{item.login}</li>)}
-      </ul>
-</>
-        
-  )
-  
-
-}
-
-export default SearchUser*/
