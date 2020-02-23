49d5fee4306231eb708ffc08de5ae9d575859962
mamathamereddy <mamathavangala1@gmail.com>
2020-02-23 23:26:51 +0100

    commit

--- react/week4/github-user-searcher/src/App.js
+++ react/week4/github-user-searcher/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header'
 import SearchUser from './components/SearchUser'
 import './App.css';
-import Renderprop from './Renderprop';
+//import Renderprop from './Renderprop';
 
 function App() {
   return (
