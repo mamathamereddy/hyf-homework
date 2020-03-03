1bdfb43326687ac623a4232b53ce34bdac260e76
mamathamereddy <mamathavangala1@gmail.com>
2020-03-03 22:53:49 +0100

    added some css to the component but need to fix with dropdown list

--- react/week5/my-app/src/components/Servet.js
+++ react/week5/my-app/src/components/Servet.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
 import Tab from "@material-ui/core/Tab";
 
+  
 function Servet() {
   const Servetbutton = { marginTop: 100 };
   return (
-    <ListItem style={Servetbutton}>
-      <Tab icon={<AccountCircleOutlinedIcon />} label="Servet" />
-    </ListItem>
-  );
+      <ListItem
+            button
+            key="Servet"
+            className={`${classes.item} ${classes.user}`}
+          >
+            <ListItemIcon className={classes.itemIcon}>
+              <AccountCircleOutlinedIcon
+                fontSize="large"
+                className={classes.icon}
+              />
+            </ListItemIcon>
+            <ListItemText
+              primary="Servet"
+              className={classes.itemText}
+              disableTypography
+            />
+          </ListItem>  );
 }
 
 export default Servet;
